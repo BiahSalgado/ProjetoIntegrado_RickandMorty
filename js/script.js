3ee051b07b859dc07102a7cd0b434b0be7a728da
Beatriz Salgado <beatriz.salgado355@gmail.com>
2024-04-20 15:58:01 -0300

    melhorias

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(function () {
 
+    console.log('teste');
+    buscaAPIPersonagens();
+
+    $("#btn_buscar").click(function() {
+        //buscaAPIPersonagens();
+    });
 
     //HEADER
     $(window).scroll(function () {
@@ -19,5 +25,43 @@ $(function () {
           }
     });
 
+    function buscaAPIPersonagens()
+    {
+        var pagina = 1;
+        var url = 'https://rickandmortyapi.com/api/character/?page=' + pagina;
+        
+        var resultado = httpGet(url);
+       
+        console.log(resultado.results);
+
+        carregaDadosTabela(resultado.results);
+    }
+
+    function carregaDadosTabela(dados)
+    {
+        var content = '';
+        
+        $.each(dados, function(index, value) {
+            
+            console.log(index + ' :  id' + value.id  + '  nome' + value.name  + ' img: ' +  value.image);
+
+            content += '<tr id="' + value.id + '">';
+            content += '<td>' +  value.id + '</td>';
+            content += '<td> <b>' + value.name  + ' </b></td>';
+            content += '<td> <img src="' +  value.image + '" alt="' +  value.name + '" width="80" height="80"></td>';
+            content += '</tr>';
+
+        });
+        
+        $('#tabelaDesenhos tbody').html(content);
+    }
+    
+    function httpGet(theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return jQuery.parseJSON(xmlHttp.responseText);
+    }
 
 });
